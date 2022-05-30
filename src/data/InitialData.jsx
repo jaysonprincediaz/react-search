38303cf8665b01b6edb1d56631668456fd7eee2b
Jayson Alihassan Diaz <95516878+jaysonprincediaz@users.noreply.github.com>
2022-05-30 19:29:51 +0800

    Search Feature

---
+++ src/data/InitialData.jsx
@@ -0,0 +1,39 @@
+export const InitialData = [
+  {
+    id: 1,
+    imgPath: "/public/assets/1.jpg",
+    name: "Aldrian Piezas",
+    email: "aldrianpiezas@gmail.com",
+    address: "Molave, Mindanao",
+  },
+  {
+    id: 2,
+    imgPath: "/public/assets/2.jpg",
+    name: "Sonnymar Diaz",
+    email: "sonnymardiaz@gmail.com",
+    address: "Nueva Ecija, Luzon",
+  },
+  {
+    id: 3,
+    imgPath: "/public/assets/3.jpg",
+    name: "Dindo Paran",
+    email: "dindoparan@gmail.com",
+    address: "Cebu, Visayas",
+  },
+  {
+    id: 4,
+    imgPath: "/public/assets/4.jpg",
+    name: "Rommel Daig",
+    email: "rommeldaig@gmail.com",
+    address: "Pagadian City, Mindanao",
+  },
+  {
+    id: 5,
+    imgPath: "/public/assets/5.png",
+    name: "Jayson Diaz",
+    email: "jaysondiaz@gmail.com",
+    address: "Pagadian, Mindanao",
+  },
+];
+
+export default InitialData;
